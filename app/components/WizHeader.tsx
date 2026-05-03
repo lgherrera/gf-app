3faad44b52cd857a6f5592a58473ca1434acb6b9
Luis Gubler <your_email@example.com>
2026-05-02 20:54:18 -0400

    increase reviewLabel font size to 16px

--- app/components/WizHeader.tsx
+++ app/components/WizHeader.tsx
@@ -5,28 +5,12 @@ import Link from 'next/link';
 import styles from './WizHeader.module.css';
 
 const logoSrc = process.env.NEXT_PUBLIC_APP_SOURCE === 'nsfw'
-  ? '/logos/wiz_header_pink.jpg'
-  : '/logos/wiz_header.jpg';
+  ? '/gf_logo.jpg'
+  : '/friends_logo.jpg';
 
 export default function WizHeader() {
   return (
     <header className={styles.header}>
-      
-      {/* Left: Home Button */}
-      <Link href="/" className={styles.iconButton} aria-label="Ir al inicio">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="currentColor"
-          width="24"
-          height="24"
-        >
-          <path d="M11.47 3.841a.75.75 0 0 1 1.06 0l8.69 8.69a.75.75 0 1 0 1.06-1.061l-8.689-8.69a2.25 2.25 0 0 0-3.182 0l-8.69 8.69a.75.75 0 1 0 1.061 1.06l8.69-8.689Z" />
-          <path d="m12 5.432 8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 0 1-.75-.75v-4.5a.75.75 0 0 0-.75-.75h-3a.75.75 0 0 0-.75.75V21a.75.75 0 0 1-.75.75H5.625a1.875 1.875 0 0 1-1.875-1.875v-6.198a2.29 2.29 0 0 0 .091-.086L12 5.432Z" />
-        </svg>
-      </Link>
-
-      {/* Middle: Logo */}
       <div className={styles.logoContainer}>
         <Link href="/">
           <img 
@@ -36,10 +20,6 @@ export default function WizHeader() {
           />
         </Link>
       </div>
-
-      {/* Right: spacer to keep logo centered */}
-      <div style={{ width: 40 }} />
-
     </header>
   );
 }
