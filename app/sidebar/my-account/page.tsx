5b2df03656c55362e17b87d969d635a634ff1f97
Luis Gubler <your_email@example.com>
2026-05-05 19:07:17 -0400

    fix: badge color follows SFW/NSFW theme

--- app/sidebar/my-account/page.tsx
+++ app/sidebar/my-account/page.tsx
@@ -29,7 +29,7 @@ export default function MyAccountPage() {
       .finally(() => setLoading(false));
   }, [userId]);
 
-  const displayValue = loading ? '...' : msisdn || 'msisdn no identificado';
+  const displayValue = loading ? '...' : msisdn || 'MSISDN no Identificado';
 
   return (
     <div className={styles.page}>
@@ -38,15 +38,17 @@ export default function MyAccountPage() {
       <div className={styles.content}>
         <div className={styles.iconWrap}>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="48" height="48">
-            <path fillRule="evenodd" d="M11.078 2.25c-.917 0-1.699.663-1.85 1.567L9.05 4.889c-.02.12-.115.26-.297.348a7.493 7.493 0 0 0-.986.57c-.166.115-.334.126-.45.083L6.3 5.508a1.875 1.875 0 0 0-2.282.819l-.922 1.597a1.875 1.875 0 0 0 .432 2.385l.84.692c.095.078.17.229.154.43a7.598 7.598 0 0 0 0 1.139c.015.2-.059.352-.153.43l-.841.692a1.875 1.875 0 0 0-.432 2.385l.922 1.597a1.875 1.875 0 0 0 2.282.818l1.019-.382c.115-.043.283-.031.45.082.312.214.641.405.985.57.182.088.277.228.297.35l.178 1.071c.151.904.933 1.567 1.85 1.567h1.844c.916 0 1.699-.663 1.85-1.567l.178-1.072c.02-.12.114-.26.297-.349.344-.165.673-.356.985-.57.167-.114.335-.125.45-.082l1.02.382a1.875 1.875 0 0 0 2.28-.819l.923-1.597a1.875 1.875 0 0 0-.432-2.385l-.84-.692c-.095-.078-.17-.229-.154-.43a7.614 7.614 0 0 0 0-1.139c-.016-.2.059-.352.153-.43l.84-.692c.708-.582.891-1.59.433-2.385l-.922-1.597a1.875 1.875 0 0 0-2.282-.818l-1.02.382c-.114.043-.282.031-.449-.083a7.49 7.49 0 0 0-.985-.57c-.183-.087-.277-.227-.297-.348l-.179-1.072a1.875 1.875 0 0 0-1.85-1.567h-1.843ZM12 15.75a3.75 3.75 0 1 0 0-7.5 3.75 3.75 0 0 0 0 7.5Z" clipRule="evenodd" />
+            <path fillRule="evenodd" d="M18.685 19.097A9.723 9.723 0 0 0 21.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 0 0 3.065 7.097A9.716 9.716 0 0 0 12 21.75a9.716 9.716 0 0 0 6.685-2.653Zm-12.54-1.285A7.486 7.486 0 0 1 12 15a7.486 7.486 0 0 1 5.855 2.812A8.224 8.224 0 0 1 12 20.25a8.224 8.224 0 0 1-5.855-2.438ZM15.75 9a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" clipRule="evenodd" />
           </svg>
         </div>
 
         <h1 className={styles.title}>Mi Cuenta</h1>
         <p className={styles.badge}>{displayValue}</p>
-        <p className={styles.description}>
-          Estamos trabajando en esta sección. Pronto podrás gestionar tu perfil, suscripción y preferencias desde aquí.
-        </p>
+        <div className={styles.unsubscribeBox}>
+          <p className={styles.unsubscribeText}>
+            PARA DESUSCRIBIR ENVÍA SALIR AL 6920
+          </p>
+        </div>
       </div>
 
       <GFFooter />
