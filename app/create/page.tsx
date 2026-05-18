ebabc0d22beb49285748bee20ef1fc1e1ebba0b9
Luis Gubler <luis@zgroup.cl>
2026-05-18 18:49:52 -0400

    Sync Outfit type with updated NSFW outfit values

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -90,7 +90,7 @@ const OUTFIT_OPTIONS_ALL = [
   { value: 'micro red bikini, tiny strings.',                                      label: 'Baywatch',       mode: 'nsfw' },
   { value: 'sexy and revealing tight dress',                                   label: 'Actriz Porno',             mode: 'nsfw' },
   { value: 'sexy and revealing tight dress on high heels',                     label: 'Top Model',             mode: 'nsfw' },
-  { value: 'sexy and revealing casual female teacher outfit',                  label: 'Profesora Ardiente',          mode: 'nsfw' },
+  { value: 'sexy and revealing casual female teacher outfit, holding a wooden pointer stick',                  label: 'Profesora Ardiente',          mode: 'nsfw' },
   { value: 'sexy and revealing casual outfit',                                 label: 'Bomba Sexy',              mode: 'nsfw' },
   { value: 'sexy and revealing female police outfit',                    label: 'Mujer Policía',      mode: 'nsfw' },
   { value: 'sexy and revealing sports outfit, revealing top, short pants',     label: 'Entrenadora Personal',   mode: 'nsfw' },
